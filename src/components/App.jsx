25fd3fdc7b1f60833255cd0418b271800f11e4d3
DianiMor <diana.3mol@gmail.com>
2024-05-08 16:02:45 -0500

    axio

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+/* import axios from 'axios'; */
 import Searchbar from './searchbar/Searchbar.jsx';
 import ImageGallery from './imageGallery/ImageGallery.jsx';
 import ImageGalleryItem from './imageGalleryItem/ImageGalleryItem.jsx';
